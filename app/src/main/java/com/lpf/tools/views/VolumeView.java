120caaae693b314c820c694c09c3acc7d1ff9b73
lpf <lpftobetheone@163.com>
2016-08-24 08:24:47 +0800

    添加音频条视图

---
+++ app/src/main/java/com/lpf/tools/views/VolumeView.java
@@ -0,0 +1,82 @@
+package com.lpf.tools.views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.media.AudioManager;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by lpf on 16/8/24.
+ * 自定义音量图
+ */
+public class VolumeView extends View{
+
+    private int mWidth;
+    private int mRectWidth;
+    private int mRectHeight;
+    private Paint mPaint;
+    private int mRectCount;
+    private int offset = 8;
+    private double mRandom;
+    private LinearGradient mLinearGradient;
+
+    public VolumeView(Context context) {
+        this(context,null);
+    }
+
+    public VolumeView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public VolumeView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    private void initView() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL);
+        mRectCount = 30;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = getWidth();
+        mRectHeight = getHeight();
+        mRectWidth = (int)(mWidth*0.8/mRectCount);
+        mLinearGradient = new LinearGradient(
+                0,0,mRectWidth,mRectHeight,Color.YELLOW,Color.RED, Shader.TileMode.CLAMP
+        );
+        mPaint.setShader(mLinearGradient);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        for (int i = 0; i < mRectCount; i++) {
+            mRandom = Math.random();
+            float currentHeight = (float)(mRectHeight*mRandom);
+            canvas.drawRect(
+                    (float) (mWidth * 0.1 + mRectWidth * i + offset),
+                    currentHeight,
+                    (float) (mWidth * 0.1 + mRectWidth * (i + 1)),
+                    mRectHeight,
+                    mPaint
+            );
+        }
+
+        postInvalidateDelayed(500);
+    }
+
+    private onVolumeListener onVolumeListener;
+    public interface onVolumeListener{
+        int setVolume();
+    }
+}
